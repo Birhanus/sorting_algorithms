99404147530746304b854923371f61b1e22bbd6c
Birhanus <birhanushimles716@gmail.com>
2022-01-11 05:26:45 -0800

    insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,35 +1,33 @@
 #include "sort.h"
 /**
- *insertion_sort_list - sort a double linked list
- *@list: double pointer to the list
- */
+* insertion_sort_list - insertion sorting algorithm
+* @list: linked list to sort
+*/
 void insertion_sort_list(listint_t **list)
 {
-listint_t *aux;
-listint_t *aux2 = *list;
-listint_t *i = *list;
+	listint_t *tmp;
+	int n;
 
-if  (aux2 == NULL)
-return;
-aux2 = aux2->next;
-while (aux2 != NULL)
-{
-i = aux2;
-aux2 = aux2->next;
-while (i->prev != NULL && i->n < i->prev->n)
-{
-if (i->next != NULL)
-i->next->prev = i->prev;
-if (i->prev->prev != NULL)
-i->prev->prev->next = i;
-else
-*list = i;
-i->prev->next = i->next;
-i->next = i->prev;
-aux = i->prev->prev;
-i->prev->prev = i;
-i->prev = aux;
-print_list(*list);
-}
-}
+	if (!list)
+		return;
+	tmp = *list;
+	while (tmp)
+	{
+		while (tmp)
+		{
+			if (tmp->next)
+			{
+				if (tmp->n > tmp->next->n)
+				{
+					n = tmp->n;
+					*(int *)&tmp->n = tmp->next->n;
+					*(int *)&tmp->next->n = n;
+					tmp = *list;
+					print_list(*list);
+					break;
+				}
+			}
+			tmp = tmp->next;
+		}
+	}
 }
